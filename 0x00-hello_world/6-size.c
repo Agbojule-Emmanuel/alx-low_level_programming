20f5be3129eff74afd11de0ba924b3b5f569591c
Agbojule-Emmanuel <agbojuleemmanuel99@gmail.com>
2022-10-13 17:19:12 -0700

    C - Hello World Tasks

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("size of a char: %i bytes(s)\n", sizeof(char));
+	printf("size of an int: %i bytes(s)\n", sizeof(int));
+	printf("size of a long: %i bytes(s)\n", sizeof(long));
+	printf("size of a long long int: %i bytes(s)\n", sizeof(long long int));
+	printf("size of a float: %i bytes(s)\n", sizeof(float));
+	return (0);	}
